7825f7249a408a76e4b2a4811e7e23247d662c47
richardjohns <richard.johns193@gmail.com>
2017-10-11 17:42:03 +1300

    still not working

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,44 @@
 import React from 'react'
+import {getRobot} from '../apiClient'
 
-const App = () => {
-  return (
-    <h1>React development has begun!</h1>
-  )
+class App extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      text: ''
+    }
+    this.updateSearch = this.updateSearch.bind(this)
+    this.sendText = this.sendText.bind(this)
+  }
+
+  updateSearch (event) {
+    this.setState({text:event.target.text})
+    console.log(this.state)
+  }
+
+  sendText (e) {
+    e.preventDefault()
+    getRobot(this.state.text, (res) => {
+      console.log('hello' + res)
+      this.setState()
+    })
+  }
+
+  render () {
+    return (
+      <div className='robot-form'>
+        <h1>Hello! Robot!</h1>
+
+        <form onSubmit={this.sendText}>
+          <input type='text' name='search' value={this.state.text} onChange={this.updateSearch} />
+          <input type="submit" value="search" />
+        </form>
+        <div>
+          <img src="http:\/\/bit.ly\/1fQxZ6w" alt=""/>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App
-
